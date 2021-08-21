ece96aeb900d9da4152d47747051fd27738a9789
avalon1993 <hqq1993619@live.com>
2021-08-21 19:17:24 +0800

    feat:原型模式

---
+++ src/com/PrototypePattern/AdvTemplate.java
@@ -0,0 +1,16 @@
+package com.PrototypePattern;
+
+public class AdvTemplate {
+
+    private String advSubject = "XX银行国庆信用卡抽奖活动";
+    private String advContext = "国庆抽奖活动通知，只要刷卡送一百万！。。。";
+
+
+    public String getAdvSubject() {
+        return this.advSubject;
+    }
+
+    public String getAdvContext() {
+        return this.advContext;
+    }
+}
---
+++ src/com/PrototypePattern/Mail.java
@@ -0,0 +1,72 @@
+package com.PrototypePattern;
+
+public class Mail implements Cloneable {
+
+    private String receiver;
+
+    private String subject;
+
+    private String appellation;
+
+    private String context;
+
+    private String tail;
+
+
+    public Mail(AdvTemplate advTemplate) {
+        this.context = advTemplate.getAdvContext();
+        this.subject = advTemplate.getAdvSubject();
+    }
+
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAppellation() {
+        return appellation;
+    }
+
+    public void setAppellation(String appellation) {
+        this.appellation = appellation;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getTail() {
+        return tail;
+    }
+
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public Mail clone() {
+        try {
+            Mail clone = (Mail) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
